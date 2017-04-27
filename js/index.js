0fea18339a92e24fdfebf55c429c23b8224953cd
Brian Mendez <brmendez@gmail.com>
2017-04-26 23:47:19 -0700

    PhotoSwipe library added for portfolio page

--- js/index.js
+++ js/index.js
@@ -4,7 +4,44 @@ $(window).load(function() {
 	});
 });
 
+function showSlide(slide) {
+	let index = slide.index()
+	, slides = [];
+
+	var pswpElement = document.querySelectorAll('.pswp')[0];
+
+	$('.gallery a').each(function(i,v) {
+		var size = $(this).attr('data-size').split('x');
+
+        // build array w/ slide objects
+		imgObj = {};
+		imgObj.src = v.href
+		imgObj.msrc = $(this).children().attr('src'); //thumbnail picture
+		imgObj.w = parseInt(size[0], 10);
+		imgObj.h = parseInt(size[1], 10);
+		imgObj.title = 'no caption';
+
+		slides.push(imgObj);
+	});
+
+	var options = {
+	    // optionName: 'option value'
+	    index: index-1 // start at selected slide
+	};
+
+	// Initializes and opens PhotoSwipe
+	var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, slides, options);
+	gallery.init();
+}
+
+
 $(function() {
-	$('#header').load('header.html');
-	$('.footer-row').load('footer.html');
+	// $('#header').load('header.html');
+	// $('.footer-row').load('footer.html');
+
+	$('.gallery a').click(function(e) {
+		e.preventDefault();
+		showSlide($(this));
+	});
+
 });
